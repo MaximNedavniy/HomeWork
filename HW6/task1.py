5e0d4e93ba9a2e73225d0291d23fcd147b66e5ff
MaximNedavniy <145386833+MaximNedavniy@users.noreply.github.com>
2023-10-02 22:27:51 +0300

    Delete HW6 directory

--- HW6/task1.py
+++
@@ -1,7 +0,0 @@
-import random
-some_list = []
-while True:
-    some_list.append(random.randint(0, 100))
-    if len(some_list) == 10:
-        break
-print(f"List: {some_list} Maximum number: {max(some_list)}")
