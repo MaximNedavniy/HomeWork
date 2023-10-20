26806bb0eeef0eed4694ac2bf698ae57079eef36
Максим <maxim.nedavniy@gmail.com>
2023-10-20 16:56:17 +0300

    complete hw6(edit task2)

--- HW6/task2.py
+++ HW6/task2.py
@@ -3,11 +3,11 @@ some_list = []
 some_list_2 = []
 some_list_3 = []
 while True:
-    some_list.append(random.randint(0, 10))
-    some_list_2.append(random.randint(0, 10))
+    some_list.append(random.randint(1, 10))
+    some_list_2.append(random.randint(1, 10))
     if len(some_list_2) == 10:
         break
-some_list_3 = list(set(some_list + some_list_2))
+some_list_3 = list(set(some_list) & set(some_list_2))
 print(f"""
 List 1: {some_list},
 List 2: {some_list_2},
